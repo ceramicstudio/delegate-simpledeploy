4a1908c2de39d9f4c2c1c667a873c9cdea977f19
Mark Krasner <markkrasner@Marks-MacBook.local>
2023-06-10 22:17:29 -0400

    Adding delegate profile models, scripts, and required files for composite deployment

--- README.md
+++ README.md
@@ -4,6 +4,12 @@ This repository contains scripts to help with the deployment of Ceramic nodes.
 
 View the guide on how to use these scripts [here](https://composedb.js.org/docs/0.4.x/guides/composedb-server/running-in-the-cloud#running-composedb-server-on-kubernetes).
 
+Once you arrive at the sentence starting with "You can now follow the existing guides" use the following steps:
+
+1. Run `npm install` within the root directory to download the required dependencies
+2. Run `npm run dev` after your dependencies have been installed
+3. You can now perform your GraphQL queries using the following endpoint: `http://localhost:5005/graphql`
+
 ## Ansible
 
 In the [ansible](ansible) directory you will find a set of Ansible playbooks to deploy Ceramic nodes on a set of servers.
---
+++ run.mjs
@@ -0,0 +1,88 @@
+import { readFileSync } from "fs";
+import { CeramicClient } from "@ceramicnetwork/http-client";
+import {
+  createComposite,
+  readEncodedComposite,
+  writeEncodedComposite,
+  writeEncodedCompositeRuntime,
+} from "@composedb/devtools-node";
+import { Composite } from "@composedb/devtools";
+import shell from "shelljs";
+import { fromString } from "uint8arrays/from-string";
+import { DID } from "dids";
+import { Ed25519Provider } from "key-did-provider-ed25519";
+import { getResolver } from "key-did-resolver";
+import ora from "ora";
+
+const spinner = ora();
+const ceramic = new CeramicClient("http://localhost:7007");
+
+//Accessing your admin key to authenticate the session
+const seed = shell.exec(
+  `kubectl get secrets --namespace ceramic ceramic-admin -o json | jq -r '.data."private-key"' | base64 -d`
+);
+const key = fromString(seed.stdout, "base16");
+const did = new DID({
+  resolver: getResolver(),
+  provider: new Ed25519Provider(key),
+});
+
+spinner.info("Authenticating ceramic admin");
+await did.authenticate();
+ceramic.did = did;
+/**
+ * @param {Ora} spinner - to provide progress status.
+ * @return {Promise<void>} - return void when composite finishes deploying.
+ */
+
+const delProfileComposite = await createComposite(
+  ceramic,
+  "./composites/01-DelegateProfile.graphql"
+);
+
+const delOfProfileSchema = readFileSync(
+  "./composites/02-DelegateOfProfile.graphql",
+  {
+    encoding: "utf-8",
+  }
+).replace("$GENERALDELEGATEPROFILE_ID", delProfileComposite.modelIDs[0]);
+
+const delOfComposite = await Composite.create({
+  ceramic,
+  schema: delOfProfileSchema,
+});
+
+const delValueComposite = await createComposite(
+  ceramic,
+  "./composites/03-DelegateGiveValue.graphql"
+);
+
+const composite = Composite.from([
+  delProfileComposite,
+  delOfComposite,
+  delValueComposite,
+]);
+
+//Writing composites to local file
+await writeEncodedComposite(composite, "./definition.json");
+spinner.info("creating composite for runtime usage");
+await writeEncodedCompositeRuntime(
+  ceramic,
+  "./definition.json",
+  "./definition.js"
+);
+spinner.info("deploying composite");
+const deployComposite = await readEncodedComposite(
+  ceramic,
+  "./definition.json"
+);
+const id = deployComposite.modelIDs;
+spinner.info(`Deployed the following models: ${id}`);
+await deployComposite.startIndexingOn(ceramic);
+spinner.succeed("composite deployed & ready for use");
+spinner.succeed("compiling composite into runtime composite");
+shell.exec(`composedb composite:compile definition.json runtime-composite.json`);
+spinner.succeed("establishing graphiql server");
+shell.exec(`composedb graphql:server --graphiql runtime-composite.json --port=5005`)
+
+
