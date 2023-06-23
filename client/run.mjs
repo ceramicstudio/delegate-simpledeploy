5ff82a1f877902efde71310309035b47da03998f
Mark Krasner <markkrasner@Marks-MacBook.lan>
2023-06-23 17:32:27 -0400

    Adding new deployment instructions and client folder for react app

---
+++ client/run.mjs
@@ -0,0 +1,58 @@
+import { readFileSync, writeFileSync } from "fs";
+import { CeramicClient } from "@ceramicnetwork/http-client";
+import {
+  createComposite,
+  readEncodedComposite,
+  writeEncodedComposite,
+  writeEncodedCompositeRuntime,
+} from "@composedb/devtools-node";
+import { Composite } from "@composedb/devtools";
+import shell from "shelljs";
+import { fromString } from "uint8arrays/from-string";
+import { DID } from "dids";
+import { Ed25519Provider } from "key-did-provider-ed25519";
+import { getResolver } from "key-did-resolver";
+import ora from "ora";
+
+const spinner = ora();
+const ceramic = new CeramicClient("http://localhost:7007");
+
+//Accessing your admin key to authenticate the session
+const seed = shell.exec(
+  `kubectl get secrets --namespace ceramic ceramic-admin -o json | jq -r '.data."private-key"' | base64 -d`
+);
+
+//Writing your admin key to local file --> I will show you how to use it later to authenticate a client
+writeFileSync("./client/admin_key.txt", seed.stdout, 'utf8');
+
+// //Creating a new admin DID based on your private key
+// const key = fromString(seed.stdout, "base16");
+// const did = new DID({
+//   resolver: getResolver(),
+//   provider: new Ed25519Provider(key),
+// });
+
+// spinner.info("Authenticating ceramic admin");
+// await did.authenticate();
+// ceramic.did = did;
+// /**
+//  * @param {Ora} spinner - to provide progress status.
+//  * @return {Promise<void>} - return void when composite finishes deploying.
+//  */
+
+// //Deploy the prebuilt composite using the same StreamIDs already pinned by HireNodes
+// spinner.info("deploying composite");
+// const deployComposite = await readEncodedComposite(
+//   ceramic,
+//   "./definition.json"
+// );
+// const id = deployComposite.modelIDs;
+// spinner.info(`Deployed the following models: ${id}`);
+// await deployComposite.startIndexingOn(ceramic);
+// spinner.succeed("composite deployed & ready for use");
+// spinner.succeed("compiling composite into runtime composite");
+// shell.exec(`composedb composite:compile definition.json runtime-composite.json`);
+// spinner.succeed("establishing graphiql server");
+// shell.exec(`composedb graphql:server --graphiql runtime-composite.json --port=5005`);
+
+
